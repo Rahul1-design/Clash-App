b1eec61a6edc98ce0d65253529fb80ff7b1cf25c
Rahul1-design <rahulshrestha3132@gmail.com>
2026-07-23 18:48:17 +0545

    feat: implement signup and login pages with UI components
    - Removed old signup page and created a new signup page with UI components.
    - Added a new login page with UI components for user authentication.
    - Integrated email sending functionality using nodemailer and ejs for welcome emails.
    - Created email templates for sending welcome messages to users.
    - Updated server configuration to handle email sending on user signup.

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+import ejs from "ejs";
+import { sendEmail } from "./config/mail.js";
 const app = express();
 const PORT = process.env.PORT || 7000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -12,7 +14,11 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
 app.get("/", async (req, res) => {
     // return res.send("We are officially starting the server😍😍!!!");
-    return res.render("Welcome", { name: "Rahul Shrestha" });
+    const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {
+        name: "Rahul Shrestha",
+    });
+    await sendEmail("bifakax491@kierko.com", "Testing SMTP", html);
+    return res.json({ msg: "Email send successfully!" });
 });
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Response, Request } from "express";
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+import ejs from "ejs";
+import { sendEmail } from "./config/mail.js";
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
@@ -17,7 +19,11 @@ app.set("views", path.resolve(__dirname, "./views"));
 
 app.get("/", async (req: Request, res: Response) => {
   // return res.send("We are officially starting the server😍😍!!!");
-  return res.render("Welcome", { name: "Rahul Shrestha" });
+  const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {
+    name: "Rahul Shrestha",
+  });
+  await sendEmail("bifakax491@kierko.com", "Testing SMTP", html);
+  return res.json({ msg: "Email send successfully!" });
 });
 
 app.listen(PORT, () => {
